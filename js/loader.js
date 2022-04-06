3eb0f2e435686c4bb8dc315d991df238bacc5944
Phuc <phuc19105@gmail.com>
2022-04-06 22:26:24 +0700

    implement cleanser and transition

--- js/loader.js
+++ js/loader.js
@@ -5,4 +5,23 @@ function boxLoader(content, parentDiv){
 		btn.appendTo(dlg);
 	}
 	dlg.appendTo(parentDiv);
+	
+	//all dialogue when first created will be hiden
+	dlg.hide();
+	animation.fadeIn(dlg);
+}
+
+
+
+
+function cleanser(parentDiv){
+	let div = $(parentDiv + '>');
+	animation.fadeOut(div);
+}
+
+
+function transition(parentDiv, next){
+	cleanser(parentDiv);
+	boxLoader(next, parentDiv);
+
 }
